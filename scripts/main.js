998fc8e6fa4dc4497c5b9869ed1057f2a35b8cfa
BboyMagik <jie22shen@gmail.com>
2021-03-07 15:55:12 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+
+let firstname = 'Jie';
+
+console.log(firstname);
+
+
+function welcome() {
+    let username = prompt("what is your name?");
+    alert ("Welcome to my website" + username);
+
+}
